55df33a8c3659ec5dd802201308f4fa31219e855
yokitheyo <razvygs@gmail.com>
2025-03-16 04:01:12 +0300

    templates static images added

--- app/main.py
+++ app/main.py
@@ -1,16 +1,44 @@
-from fastapi import FastAPI, Query, Depends
-from typing import Optional
-from datetime import date
-from pydantic import BaseModel
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.users.router import router as router_users
 from app.bookings.router import router as router_bookings
 from app.hotels.router import router as router_hotels
 from app.hotels.rooms.router import router as router_rooms
 
+from app.pages.router import router as router_pages
+from app.images.router import router as router_images
+
 app = FastAPI()
 
-app.include_router(router_rooms)
-app.include_router(router_hotels)
+app.mount("/static", StaticFiles(directory="app/static"), "static")
+
 app.include_router(router_users)
+app.include_router(router_hotels)
+app.include_router(router_rooms)
 app.include_router(router_bookings)
+
+app.include_router(router_pages)
+app.include_router(router_images)
+
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"],
+    allow_headers=[
+        "Content-Type",
+        "Set_Cookie",
+        "Access-Control-Allow-Headers",
+        "Access-Control-Allow-Origin",
+        "Authorization",
+    ],
+)
